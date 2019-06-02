300429563c97095b09ff413a3ed30420c88971bc
CesarRivasP <cesarjose58@gmail.com>
2019-06-02 18:04:37 -0400

    added componentDidMount and api call of news

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  state = {
+    news: []
+  }
+
+  componentDidMount(){
+    this.consultNews();
+  }
+
+  consultNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=ve&category=business&apiKey=1c8bbef6a2b344ac9044935d122d88bf`;
+
+    const response = await fetch(url);
+
+    const news = await response.json();
+
+    this.setState({
+      news: news.articles
+    })
+  }
+
   render(){
     return (
       <div className="App">
