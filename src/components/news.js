185ab8a671a534a4b86a9b6e85d2bfa03b3200f0
CesarRivasP <cesarjose58@gmail.com>
2019-06-04 09:06:55 -0400

    change button

--- src/components/news.js
+++ src/components/news.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 
 const News = ({ currentNew }) => {
 
@@ -30,11 +32,18 @@ const News = ({ currentNew }) => {
         </div>
 
         <div className="card-action">
-          <a href={url} target="_blank" rel="noopener noreferrer" className="waves-effect waves-light btn">Ver Noticia</a>
+          {/* <a href={url} target="_blank" rel="noopener noreferrer" className="waves-effect waves-light btn">Ver Noticia</a> */}
+          <Button href={url} variant="outlined" style={{ color: 'black', display: 'flex', justifyContent: 'center', marginRight: 0}}>
+            Ver Noticia
+          </Button>
         </div>
       </div>
     </div>
   );
+}
+
+News.propTypes = {
+  currentNew: PropTypes.object.isRequired
 }
 
 export default News;
