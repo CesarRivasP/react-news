f1be6e2f55f6b88f391eb9c5bfb5ccbd22bd77f6
CesarRivasP <cesarjose58@gmail.com>
2019-06-04 09:06:30 -0400

    implemented proptypes

--- src/components/news-list.js
+++ src/components/news-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import News from './news';
 
 const NewsList = ({ news }) => (
@@ -10,5 +11,9 @@ const NewsList = ({ news }) => (
     }
   </div>
 )
+
+NewsList.propTypes = {
+  news: PropTypes.array.isRequired
+}
 
 export default NewsList;
