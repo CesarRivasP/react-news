4f60f83ab539347a1e5fa8b3eab78336ff407da5
CesarRivasP <cesarjose58@gmail.com>
2019-06-04 07:56:07 -0400

    implemented form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Header from './components/header';
 import NewsList from './components/news-list';
+import Form from './components/form';
 
 
 class App extends Component {
@@ -12,8 +13,8 @@ class App extends Component {
     this.consultNews();
   }
 
-  consultNews = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=ve&category=business&apiKey=1c8bbef6a2b344ac9044935d122d88bf`;
+  consultNews = async ( category = 'general' ) => {  //valor por defecto
+    const url = `https://newsapi.org/v2/top-headlines?country=ve&category=${category}&apiKey=1c8bbef6a2b344ac9044935d122d88bf`;
 
     const response = await fetch(url);
 
@@ -30,6 +31,8 @@ class App extends Component {
         <Header title="Noticias" />
 
         <div className="container white news-container">
+          <Form consultNews={this.consultNews}/>
+
           <NewsList news={this.state.news}/>
         </div>
       </Fragment>
