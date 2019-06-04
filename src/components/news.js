15e3d467e94312f1b5eff410bcb2f626b08f0cf6
CesarRivasP <cesarjose58@gmail.com>
2019-06-04 07:14:19 -0400

    Added condition for images

--- src/components/news.js
+++ src/components/news.js
@@ -5,15 +5,25 @@ const News = ({ currentNew }) => {
   // extraer los datos
   const { title, description, source, urlToImage, url } = currentNew
 
+  // condicion para cargar la imagen en caso de que la haya o no
+  const image = (urlToImage) ?
+                  (
+                    <div className="card-image">
+                      <img
+                        src={urlToImage}
+                        alt={title}  //texto alternativo
+                        className=""/>
+                        <span className="card-title">{source.name}</span> {/* Nombre de quien oublica la noticia */}
+                    </div>
+                  )
+                  :
+                  null
+
+
   return (
     <div className="col s12 m6 l4">
       <div className="card">
-        <div className="card-image">
-          <img
-            src={urlToImage}
-            alt={title}  //texto alternativo
-            className=""/>
-        </div>
+        { image }
         <div className="card-content">
           <h3>{title}</h3>
           <p>{description}</p>
